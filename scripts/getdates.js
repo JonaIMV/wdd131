35cebd3c3780e6a60ce73a3e4c1272b3f1db3b5a
JonaIMV <jonaimv@gmail.com>
2025-05-08 21:00:58 -0600

    1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-const currentyear = new Date().getFullYear
+const currentYear = new Date().getFullYear
 document.getElementById("year").textContent = { currentyear };
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
